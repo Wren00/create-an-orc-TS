a9f12ef922b0bde04aed5d74e0460f69bc7538a4
Wren00 <loren.walsh@unosquare.com>
2025-07-07 12:14:12 +0100

    Committing files left out in error.

---
+++ database/migrations/V1.3__seed_images.sql
@@ -0,0 +1,8 @@
+INSERT INTO background_images (image_name, url) VALUES
+                                                    ('background', 's3://create-an-orc-images/Misc Images/Background.png'),
+                                                    ('orc_base', 's3://create-an-orc-images/Misc Images/Orc_Base.png');
+
+INSERT INTO orc_images (head, body, legs) VALUES
+                                              (1, 3, 5),
+                                              (2, 4, 3),
+                                              (3, 1, 2);
