19012cd9780de2da08f3f8422e8200bd126ab37e
Wren00 <loren.walsh@unosquare.com>
2025-08-05 11:28:10 +0100

    Adding changes for use of a dockerised container for tests - tests are all currently passing as expected.

--- server/test/global-teardown.js
+++ server/test/global-teardown.js
@@ -3,5 +3,5 @@ const { execSync } = require('child_process');
 module.exports = async () => {
     console.log("Tearing down test DB...");
 
-    execSync("docker compose -f ./docker-compose.test.yml down -v", { stdio: "inherit" });
+    execSync("docker compose -f ../docker-compose.test.yml down -v", { stdio: "inherit" });
 };
